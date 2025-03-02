8054fe2b5ad3c2ed3b276aeae01aaf440441790c
Vireth123 <virethnygren@gmail.com>
2025-03-02 19:08:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,37 @@
 const express = require("express");
-const fs = require("fs");
 const http = require("http");
 const socketIO = require("socket.io");
+const axios = require("axios");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 const PORT = process.env.PORT || 3000;
 
+const JSON_BIN_API = "https://api.jsonbin.io/v3/b/"; // Free storage
+const BIN_ID = "67c49e06e41b4d34e49f8514 "; // I'll tell you how to get this 👇
+const SECRET_KEY = "$2a$10$i7tVoPiLHzjO3bT3En4JZOunPf75qkN3qsv4CxtOBKHOHwzR92R1m";
+
 app.use(express.static("public"));
 
-io.on("connection", (socket) => {
-  console.log("New user connected");
+io.on("connection", async (socket) => {
+  console.log("User connected");
 
-  let messages = fs.readFileSync("./data/messages.txt", "utf-8").split("\n").filter(m => m);
-  messages.forEach((msg) => socket.emit("message", msg));
+  let { data } = await axios.get(`${JSON_BIN_API}${BIN_ID}`, {
+    headers: { "X-Master-Key": SECRET_KEY },
+  });
 
-  socket.on("message", (msg) => {
-    fs.appendFileSync("./data/messages.txt", msg + "\n");
+  data.record.forEach((msg) => socket.emit("message", msg));
+
+  socket.on("message", async (msg) => {
+    data.record.push(msg);
+    await axios.put(`${JSON_BIN_API}${BIN_ID}`, { record: data.record }, {
+      headers: { "X-Master-Key": SECRET_KEY },
+    });
     io.emit("message", msg);
   });
 });
 
-server.listen(PORT, () => console.log(`Chat live at http://localhost:${PORT}`));
+server.listen(PORT, () =>
+  console.log(`Chat live at http://localhost:${PORT}`)
+);
